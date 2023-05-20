ded56fd9db08ca8bfac553c94b5e170cd98a83c1
adamnetro <64706091+adamnetro@users.noreply.github.com>
2023-05-20 16:58:21 +0100

    Add files via upload

---
+++ script.js
@@ -0,0 +1,26 @@
+const Btn = document.querySelector('.dow');
+const URLinput = document.querySelector('.box');
+
+const fondUrl = /https:\/\/(www\.)youtube\.com\//;
+
+Btn.addEventListener('click', function() {
+  if (URLinput.value.match(fondUrl)) {
+    sendURL(URLinput.value);
+  }
+});
+
+function sendURL(URL) {
+  if (URLinput.value){
+  window.location.href = `http://localhost:4000/download?URL=${URL}`;
+  }
+}
+
+Btn.addEventListener('click', () => {
+  if (!URLinput.value) {
+    alert("كضحك عليا و دخل رابط");
+  }
+  if (!URLinput.value.match(fondUrl) && URLinput.value) {
+    alert("و صاحبي هذا راح مشي رابط ديال يويتيوب");
+  }
+});
+
