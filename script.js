e120e15397d4f0feec1ce92452fe636cb8c33d48
adamnetro <64706091+adamnetro@users.noreply.github.com>
2023-05-27 16:30:57 +0100

    Add files via upload

--- script.js
+++ script.js
@@ -9,13 +9,24 @@ Btn.addEventListener('click', function() {
   }
 });
 
-function sendURL(URL) {
-  if (URLinput.value){
-  window.location.href = `https://apple-concise-sofa.glitch.me/download?URL=${URL}`;
-  }
-}
+
+let boxFileName = document.querySelector('.itm-add-nmfile')
+let boxFileNameBTN = document.querySelector('.itm-add-nmfile form a')
+let boxFileNameBOX = document.querySelector('.itm-add-nmfile form input')
 let mess = document.querySelector('.mess');
 let messText = document.querySelector('.mess .txt');
+
+function sendURL(URL) {
+  if (URLinput.value && STT === false){
+  window.location.href = `http://localhost:4000/download?URL=${URL}`;
+  }
+  if (URLinput.value && STT === true){
+    window.location.href = `http://localhost:3000/download?URL=${URL}`;
+    }
+}
+
+
+
 
 Btn.addEventListener('click', () => {
   if (!URLinput.value) {
@@ -33,4 +44,63 @@ Btn.addEventListener('click', () => {
     },3000)
   }
 });
+
+
+
+const BtnTPFL = document.querySelectorAll('.type-file aside a')
+var DFstyle;
+
+BtnTPFL.forEach(BtnTPFL => {
+  BtnTPFL.addEventListener('click', () => {
+    if(DFstyle){
+      DFstyle.style = ""
+    }
+    BtnTPFL.style = "background-color: rgb(76, 0, 216); border-radius: 7px;"
+    DFstyle = BtnTPFL;
+  })
+})
+
+const BtnVD = document.querySelector('.type-file aside .VD');
+const BtnAD = document.querySelector('.type-file aside .AD');
+const STvideo = document.querySelector('.ST-video');
+const STaudio = document.querySelector('.ST-audio');
+
+let STT = false;
+
+BtnVD.addEventListener('click', () => {
+  if(!STT){
+  STvideo.style.display = "block"
+  STaudio.style.display = "none"
+  STT = true;
+}
+})
+
+BtnAD.addEventListener('click', () => {
+  if(STT){
+  STvideo.style.display = "none"
+  STaudio.style.display = "block"
+  STT = false;
+}
+})
+
+window.onload = function() {
+  BtnVD.click()
+}
+
+
+const openST = document.querySelector('.openST');
+const bkTPFL = document.querySelector('.bck-type-file');
+const clsST = document.querySelector('.clsST');
+
+openST.addEventListener('click', () => {
+  bkTPFL.style.display = "flex"
+  openST.style.display = "none"
+  clsST.style.display = "block"
+})
+
+clsST.addEventListener('click', () => {
+  bkTPFL.style.display = "none"
+  openST.style.display = "block"
+  clsST.style.display = "none"
+})
 
